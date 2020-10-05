6805977c4c43241d5584cf4d8f599280060cb99f
Vladzensan <vladzensan@gmail.com>
2020-10-05 16:05:15 +0300

    Improve time generation logic

--- DateTimeGenerator/DateTimeGenerator.cs
+++ DateTimeGenerator/DateTimeGenerator.cs
@@ -9,7 +9,7 @@ namespace DateTimeGenerator
         public object Generate(Type type)
         {
             return new DateTime(_random.Next(0, DateTime.Now.Year),
-                _random.Next(1, 12), _random.Next(1, 28));
+                _random.Next(1, 12), _random.Next(1, 30));
         }
 
         public bool IsGeneratable(Type type)
